9760194548bea25c005fafe08d3164c6c4dc1cf9
Jan Koupil <jan.koupil@gmail.com>
2023-09-13 10:28:28 +0200

    2023-09-13 opakovani - ternarni op, formatovaci retezec

---
+++ 00-Rev-Demo-Formatovaci-retezec/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _00_Rev_Demo_Formatovaci_retezec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double i = 3.14159;
+            double e = 2.71828;
+
+            //jako interpolace řetězců
+            Console.WriteLine($"Zaokrouhleno: i = {i:0.000} , e = {e:0.000}");
+            Console.WriteLine($"Doplněno zleva nulami a mezerou: i = {i:0 000.0}, e = {e:0 000.0}");
+
+            //jako formatting sting
+            Console.WriteLine("Zaokrouhleno: i = {0:0.000} , e = {1:0.000}", i, e);
+            Console.WriteLine("Doplněno zleva nulami a mezerou: i = {0:0 000.0}, e = {1:0 000.0}", i, e);
+
+        }
+    }
+}
---
+++ 00-Rev-Demo-Ternarni-operator/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _00_Rev_Demo_Ternarni_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej celé číslo: ");
+            int a = int.Parse(Console.ReadLine());
+
+            //místo
+            //if (a % 2 == 0)
+            //    Console.WriteLine("číslo je sudé");
+            //else
+            //    Console.WriteLine("číslo je liché");
+
+            //můžeme psát
+            string vysledek = a % 2 == 0 ? "sudé" : "liché";
+            Console.WriteLine($"Číslo je {vysledek}.");
+
+        }
+    }
+}
