d7e5f5d7b272d876e8064a3d93799b2f09f99f20
Jan Koupil <jan.koupil@gmail.com>
2023-10-04 10:08:04 +0200

    2023-10-04 OOP - opak - role

---
+++ 01-OOP-010-Role/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01_OOP_010_Role
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+                Role role = new Role("červená", -50);
+                Console.WriteLine(role);
+            }
+            catch
+            {
+                Console.WriteLine("Chyba při tvorbě role");
+            }
+
+            try
+            {
+                Role role = new Role("modrá", 250);
+                Console.WriteLine(role);
+                role.Delka = -80;
+                Console.WriteLine(role);
+                
+            }
+            catch
+            {
+                Console.WriteLine("Chyba při práci s rolí");
+            }
+
+            Role r = new Role();
+            
+        }
+    }
+}
---
+++ 01-OOP-010-Role/Role.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OOP_010_Role
+{
+    internal class Role
+    {
+        public string Barva { get; private set; } //automatická vlastnost
+        //public string Barva;
+        //public double Delka { get; private set; }
+
+        private double _delka; //úplná vlastnost
+        public double Delka
+        {
+            get { return _delka; }
+
+            set
+            {
+                if (value < 0) //zkontroluju, že neukládám pitomost
+                    throw new ArgumentOutOfRangeException();
+
+                _delka = value; //chráněným způsobem zapíšu
+            }
+        }
+
+        public Role(string barva, double delka)
+        {
+            Barva = barva;
+            Delka = delka;
+        }
+
+        public Role() //přetížení konstruktoru - stejný název, jiná sada parametrů
+        {
+            Barva = "";
+            Delka = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Role papíru, barva {Barva}, zbývá {Delka} cm";
+        }
+
+    }
+}
