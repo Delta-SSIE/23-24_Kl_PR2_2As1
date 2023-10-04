98e5ec6d0ebf9af6b510c77348ed584f3873165a
Jan Koupil <jan.koupil@gmail.com>
2023-10-04 10:34:46 +0200

    2023-10-04 OOP Tachometr

---
+++ 01-OOP-020-Tachometr/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01_OOP_020_Tachometr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vytvořte nový tachometr
+            Tachometr t = new();
+            Console.WriteLine($"Celkem bylo najeto {t.Stav} km.");
+
+            //zkuste jeho stav několikrát zvýšit a pak vypsat
+
+            while (true)
+            {
+                Console.Write("Kolik jsi najel: ");
+                string vstup = Console.ReadLine();
+
+                if (vstup.ToLower() == "stop")
+                    break;
+
+                try
+                {
+                    int vzdalenost = int.Parse(vstup);
+                    t.Ujed(vzdalenost);
+                    Console.WriteLine($"Celkem bylo najeto {t.Stav} km.");
+
+                }
+                catch
+                {
+                    Console.WriteLine("Nezdařilo se ujet vzdálenost. Dává smysl?");
+                }
+
+            }
+
+        }
+    }
+}
