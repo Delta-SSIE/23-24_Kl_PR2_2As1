6a2b76c5c3c0d2030443b95a09f2f929e6bf4ca1
Jan Koupil <jan.koupil@gmail.com>
2024-04-02 18:53:06 +0200

    2024-04-03 C# a DB

---
+++ 05-Reference-Library/Matika.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Reference_Library
+{
+    public class Matika
+    {
+        public static int Secti(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ 05-Reference-Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using _05_Reference_Library;
+
+namespace _05_Reference_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Matika.Secti(2, 3));
+        }
+    }
+}
